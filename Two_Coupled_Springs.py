89851db019b493c7e39dcaa3a79f5ea69e8f9b08
Dennis Rashid <151940745+DennisRashid@users.noreply.github.com>
2025-03-30 15:05:39 +0300

    Updated Coupled Spring System.

--- Two_Coupled_Springs.py
+++ Two_Coupled_Springs.py
@@ -3,29 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
 
+# Creating a function that returns the derivatives of the Coupled springs system
 def f(t, y, args):
     m1, k1, g1, m2, k2, g2 = args
 
     return [y[1], (k2/m1)*(y[2]-y[0])-(k1/m1)*y[0]-(g1/m1)*y[1], y[3], (-g2/m2)*y[3]-(k2/m2)*(y[2]-y[0])]
-    
-m1, k1, g1 = 1.0, 10.0, 0.5
-m2, k2, g2 = 2.0, 40.0, 0.25
-args = (m1, k1, g1, m2, k2, g2)
-y0 = [1.0, 0, 0.5, 0]
-t = np.linspace(0, 20, 1000)
 
+# Initial conditions
+m1, k1, g1 = 1.0, 10.0, 0.5 # mass, spring constant, gamma for the first spring
+m2, k2, g2 = 2.0, 40.0, 0.25 # mass, spring constant, gamma for the second spring
+args = (m1, k1, g1, m2, k2, g2) # Arguments for the system of equations
+y0 = [1.0, 0, 0.5, 0] # Initial positions and velocities of the two masse
+t = np.linspace(0, 20, 1000) # Time array
+
+# Solving the system of equations using scipy's ode
 r = integrate.ode(f)
 r.set_integrator('lsoda')
 r.set_initial_value(y0, t[0])
 r.set_f_params(args)
 
-dt = t[1] - t[0]
-y = np.zeros((len(t), len(y0)))
+dt = t[1] - t[0] # Time step
+y = np.zeros((len(t), len(y0))) # Array to store the results
 idx = 0
 
 while r.successful() and r.t<t[-1]:
-    y[idx, :] = r.y
-    r.integrate(r.t + dt)
+    y[idx, :] = r.y # Store the current state
+    r.integrate(r.t + dt) # Integrate the system
     idx += 1
 
 
